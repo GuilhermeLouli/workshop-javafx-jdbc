fcf5da0284efe95f4061a3d323ae9535572f9a01
Guilherme Louli <guilhermeloulisoares@gmail.com>
2022-03-10 14:49:20 -0300

    Local project created

---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module workshop_javafx_jdbc {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+
+	opens application to javafx.graphics, javafx.fxml;
+	opens gui to javafx.graphics, javafx.fxml;
+	opens model.entities to javafx.graphics, javafx.fxml, javafx.base;
+}
